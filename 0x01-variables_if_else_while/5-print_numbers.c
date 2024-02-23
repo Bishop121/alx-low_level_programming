67d03f5b06bafa9476e4523effef61fe067e515a
Bishop121 <obidikeemmanuel@outlook.com>
2024-02-23 18:39:32 +0100

    this commit

--- 0x01-variables_if_else_while/5-print_numbers.c
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -2,20 +2,19 @@
 
 
 /**
- * main - program that prints the alphabet in lowercase and upper case
+ * main - Write a program that prints all single digit numbers of base 10 starting from 0
  *
  * Return: Always 0.
  */
 
 int main(void)
 {
-	int  number;
-
-	for (number = 0; number <= 10; number++)
-		
-	{
-		printf("%d \n", number);
-	}		
-
+	char  number;
+	
+       for (number = 0; number <= 9; number++)
+       {
+		printf("%d", number);
+       }
+    	printf("\n");   
 	return (0);
 }
